27845e4406459bb017a6ba911738877cdaaa5cae
Nakacwa Olivia <k.nakacwa.olivia@gmail.com>
2022-11-11 18:25:26 +0300

    Connected to db

--- app.py
+++ app.py
@@ -1,108 +1,108 @@
 # from msilib import init_database
-from flask import Flask, render_template, url_for, request, flash
+from flask import Flask, render_template, url_for, request, flash, session, redirect
 from flask_sqlalchemy import SQLAlchemy
 import psycopg2
 import re
 import psycopg2.extras
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-DB_HOST = 'localhost'
-DB_NAME = 'Doctor-system'
-DB_USER = 'postgres'
-DB_PASS = '@Wdsk5313'
+app.secret_key = '@Wdsk5313'
+
+DB_HOST = "localhost"
+DB_NAME = "Doctor-system"
+DB_USER = "postgres"
+DB_PASS = "@Wdsk5313"
 
 conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER,
                         password=DB_PASS, host=DB_HOST)
 
-# ENV = 'dev'
-
-# if ENV == 'dev':
-#     app.debug = True
-#     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgresql:@Wdsk5313/Doctor-system'
-# else:
-#     app.debug = False
-#     app.config['SQLALCHEMY_DATABASE_URI'] = ''
-
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# db = SQLAlchemy(app)
-
-# class register(db.Model):
-#     __tablename__ = 'Register'
-#     userName = db.Column(db.String(200), primary_key=True)
-#     firstName = db.Column(db.String(200))
-#     lastName = db.Column(db.String(200))
-#     password = db.column(db.String(100))
-#     age = db.Column(db.Integer)
-#     contact = db.Column(db.Integer, unique=True)
-#     email = db.Column(db.String(200), unique=True)
-#     country = db.Column(db.String(200))
-
-#     def __init__(self, firstName, lastName, password, age, contact, email, country, remote_addr=None,):
-#         self.firstName = firstName
-#         self.lastName = lastName
-#         self.password = password
-#         self.age = age
-#         self.contact = contact
-#         self.email = email
-#         self.country = country
-
-#         # if remote_addr is None and has_request_context():
-#         #         remote_addr = request.remote_addr
-#         # self.remote_addr = remote_addr
 
 
 @app.route('/')
 def home():
+    if 'loggedin' in session:
+        # User is loggedin show them the home page
+        return render_template('home.html', username=session['username'])
+    # User is not loggedin redirect to login page
     return render_template('index.html')
 
 
-# @app.route('/login', methods=['GET'])
-# def login():
-#     if request.method == 'GET':
-#         userName = request.form['userName']
-#         password = request.form['password']
-
-#         if userName == '' or password == '':
-#             return render_template('login.html', message='Please enter required fields')
-#         return render_template('index.html')
+@app.route('/login/', methods=['GET'])
+def login():
+    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    # Check if "username" and "password" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        print(password)
+ 
+        # Check if account exists using MySQL
+        cursor.execute('SELECT * FROM users WHERE username = %s', (username,))
+        # Fetch one record and return result
+        account = cursor.fetchone()
+ 
+        if account:
+            password_rs = account['password']
+            print(password_rs)
+            # If account exists in users table in out database
+            if check_password_hash(password_rs, password):
+                # Create session data, we can access this data in other routes
+                session['loggedin'] = True
+                session['id'] = account['id']
+                session['username'] = account['username']
+                # Redirect to home page
+                return redirect(url_for('index'))
+            else:
+                # Account doesnt exist or username/password incorrect
+                flash('Incorrect username/password')
+        else:
+            # Account doesnt exist or username/password incorrect
+            flash('Incorrect username/password')
+ 
+    return render_template('login.html')
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    if request.method == 'POST':
-        if 'firstName' in request.form and 'lastName' in request.form and 'password' in request.form and 'age' in request.form and 'contact' in request.form and 'email' in request.form and 'country' in request.form:
-            firstName = request.form['firstName']
-            lastName = request.form['lastName']
-            password = request.form['password']
-            age = request.form['age']
-            contact = request.form['contact']
-            email = request.form['email']
-            country = request.form['country']
-
-            _hashed_password = generate_password_hash(password)
-
-            cursor.execute('SELECT * FROM register where contact = %d', (contact))
-            account = cursor.fetchone()
-            print(account)
-
-            if account:
-                flash('Account already exists')
-            elif not re.match(r'[^@]+@[^@]+\.[^@]+',email):
-                flash('Invalid email address')
-            elif not contact or not email or not password:
-                flash('Please fill out the form')
-            else:
-                cursor.execute("INSERT INTO register (firstName, lastName, password, age, contact, email, country) VALUES(%S, %S, %S, %d, %d, %S, %s)", (firstName, lastName, _hashed_password, age, contact, email, country))
-                conn.commit()
-                flash('You have successfully registered')
+    # Check if "username", "password" and "email" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'firstname' in request.form and 'lastname' in request.form and 'password' in request.form and 'age' in request.form and 'contact' in request.form and 'email' in request.form and 'country' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+        password = request.form['password']
+        age = request.form['age']
+        contact = request.form['contact']
+        email = request.form['email']
+        country = request.form['country']
+    
+        _hashed_password = generate_password_hash(password)
+ 
+        #Check if account exists using MySQL
+        cursor.execute('SELECT * FROM users WHERE username = %s', (username,))
+        account = cursor.fetchone()
+        print(account)
+        # If account exists show error and validation checks
+        if account:
+            flash('Account already exists!')
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            flash('Invalid email address!')
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            flash('Username must contain only characters and numbers!')
+        elif not username or not password or not email:
+            flash('Please fill out the form!')
+        else:
+            # Account doesnt exists and the form data is valid, now insert new account into users table
+            cursor.execute("INSERT INTO register (username, firstName, lastName, password, age, contact, email, country) VALUES(%s, %S, %S, %S, %d, %d, %S, %s)", (username, firstname, lastname, _hashed_password, age, contact, email, country))
+            conn.commit()
+            flash('You have successfully registered!')
     elif request.method == 'POST':
-        flash('Please fill out the form')
-
-    return render_template('signup.html')
-
-
+        # Form is empty... (no POST data)
+        flash('Please fill out the form!')
+    # Show registration form with message (if any)
+    return render_template('register.html')
+    
 if __name__ == '__main__':
     app.run(debug=True)
